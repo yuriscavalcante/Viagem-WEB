f8409297fb106284781c13fbd01cb7194c3d9828
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2023-05-31 19:53:28 -0300

    app name changed to Viagem-WEB

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'appointment-scheduling',
+  appName: 'Viagem-WEB',
   webDir: 'www',
   server: {
     androidScheme: 'https'
