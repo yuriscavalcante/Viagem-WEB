cfb72e697b76e2fe74e7d3df7922a5bb159e7e4a
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2023-06-01 18:56:09 -0300

    feat(Menu): logout placed in the menu component

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
-import { loading } from 'src/app/shared/loading';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +6,7 @@ import { loading } from 'src/app/shared/loading';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-  private loading = loading();
+ 
   public arr = [
     {
       name: 'teste 1'
@@ -31,18 +28,9 @@ export class HomePage implements OnInit {
     'Finalizado', 'Agendado', 'Em Aguardo'
   ]
   constructor(
-    private router: Router,
-    private authService: AuthService
   ) { }
 
   ngOnInit() {
-  }
-
-  async logout(event: any){
-    (await this.loading).present();
-    await this.authService.logout();
-    (await this.loading).dismiss();
-    await this.router.navigate(['login']);
   }
 
 }
