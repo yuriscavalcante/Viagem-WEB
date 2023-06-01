3411775380ef72a94b7c89b74f12bef8ebca7486
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2023-06-01 18:19:22 -0300

    feat(guard): removed for now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
-    canActivate: [AuthGuard]
+    // canActivate: [AuthGuard]
   },
   {
     path: 'login',
